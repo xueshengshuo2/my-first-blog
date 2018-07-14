8b5b4c39b863d4b856e8727fe8ff66f99c91644d
xueshengshuo2 <xueshengshuo@163.com>
2018-07-14 10:51:53 +0800

    add comments

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-urlpatterns = [
-    # Examples:
-    # url(r'^$', 'mysite.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+from django.contrib.auth import views
 
+urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/login/$', views.login, name='login'),
+    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
     url(r'', include('blog.urls')),
 
 ]
